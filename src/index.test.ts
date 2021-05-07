dfcb12f42723b9e647963832681de1babca12f9a
Kunuk Nykjær <knyk@novonordisk.com>
2021-05-07 13:26:39 +0200

    upd test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,24 @@
-import { enableScrollLock, disableScrollLock, isScrollLockEnabled } from './';
+import {
+  enableScrollLock,
+  disableScrollLock,
+  isScrollLockEnabled,
+  getScrollbarWidth,
+} from './';
 
 describe('bodyScrollLock', () => {
   test('enable is a function', () => {
     expect(typeof enableScrollLock).toBe('function');
   });
+
   test('disable is a function', () => {
     expect(typeof disableScrollLock).toBe('function');
   });
+
   test('isEnabled is a function', () => {
     expect(typeof isScrollLockEnabled).toBe('function');
+  });
+
+  test('getScrollbarWidth is a function', () => {
+    expect(typeof getScrollbarWidth).toBe('function');
   });
 });
